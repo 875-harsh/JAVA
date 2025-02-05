7fc18e99f6a7ef67c7c0bad7821b8fd9b6c09c58
HARSH JANGHEL <harshjanghel875@gmail.com>
2025-02-05 08:12:53 +0530

    Add files via upload

---
+++ challenge3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class challenge3 {
+  public static void main(String[] args) {
+    Scanner input= new Scanner(System.in);
+    System.out.print("enter your name:");
+    String name=input.nextLine();
+    System.out.print("Welcome " +name);
+    System.out.print(" to KG coding");
+  }
+  
+}
