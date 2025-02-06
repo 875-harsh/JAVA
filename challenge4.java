60e5be8a09d7c1a1825ccda598642c5acda649e3
HARSH JANGHEL <harshjanghel875@gmail.com>
2025-02-06 08:39:56 +0530

    Add files via upload

---
+++ challenge4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class challenge4 {
+  public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    System.out.print("enter no.1:");
+    int a=input.nextInt();
+    System.out.print("enter no.2:");
+    int b=input.nextInt();
+    System.out.println("Sum:"+(a+b));
+  }
+  
+}
