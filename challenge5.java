a3528f7b4bea6255c6a2f3e597bc58bb8998e6e6
HARSH JANGHEL <harshjanghel875@gmail.com>
2025-02-07 08:36:15 +0530

    Add files via upload

---
+++ challenge5.java
@@ -0,0 +1,12 @@
+public class challenge6 {
+  public static void main(String[] args) {
+    boolean n=true;
+    String name="ritu";
+    if(n){
+      System.out.println("ms."+name);
+    }else{
+      System.out.println("mr."+name);
+    }
+
+  }
+}
